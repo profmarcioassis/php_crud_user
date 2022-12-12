60056d68fe1a9c0e54660878e8482ae15fc4fa57
Márcio Assis Miranda <marciomiranda@MacBook-Pro-de-Marcio.local>
2022-12-12 14:18:44 -0300

    12-12

--- index.php
+++ index.php
@@ -9,11 +9,6 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>CRUD Pessoa - LOGIN</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
         <script type="text/javascript">
             $(document).ready(function () {
                 setInterval(function () {
@@ -85,6 +80,11 @@ session_start();
                 </div>
         </div>
         </form>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
 
 </html>
