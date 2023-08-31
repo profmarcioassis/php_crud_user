b1f374231611de4389617853bfc1523784592bbf
Márcio Assis Miranda <assismiranda@gmail.com>
2023-08-31 10:52:53 -0300

    commit mac 28-08

--- menu.php
+++ menu.php
@@ -1,17 +1,3 @@
-<!-- <script>
-    $(document).ready(function() {
-        $(".nav-item").click(function(event) {
-            event.preventDefault();
-            $(this).addClass("active").siblings().removeClass("active");
-        });
-    });
-</script> -->
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
 <div class="align-middle" style="height: 50px; width: 100%; margin-top: 10px; padding-right: 10px; padding-top: 10px; background-color: silver; text-align: right;">
     Bem-vindo(a): <b> <?php echo $_SESSION["nome"]; ?></b>
     &nbsp;&nbsp;<a href="sair.php" title="Logout" style="text-decoration: none;">Sair <i class="fa fa-sign-out"></i></a>
@@ -47,3 +33,4 @@
         </li>
     </ul>
 </nav>
+
